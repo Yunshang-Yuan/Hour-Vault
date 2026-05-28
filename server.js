6b71e642b8658f922c6a1c9dc7b58aacc2ffd507
Yunshang-Yuan <atlzxz813@gmail.com>
2026-05-28 09:12:45 +0800

    前端框架

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('欢迎来到 Hour Vault 时间银行！服务器已成功运行！');
-});
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 告诉服务器：把 /public 文件夹里的东西当成静态网站公开出去！
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Hour Vault 服务器已通过 Express 启动，端口 3000');
 });
